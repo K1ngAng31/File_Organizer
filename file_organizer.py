81779d161381e286c62286838ed0cd64e0ffc157
Angel Santana <47339457+K1ngAng31@users.noreply.github.com>
2021-01-04 17:48:04 -0700

    Add files via upload

---
+++ file_organizer.py
@@ -0,0 +1,90 @@
+import os
+import collections
+from pprint import pprint
+import sys
+import time
+import platform
+
+def organize():
+	SYS_ARG_PATH = os.path.normpath(sys.argv[1]) #input path for directory to organize
+
+	# File type to folder
+	DIRECTORIES = collections.defaultdict()
+	for name_of_file in os.listdir(SYS_ARG_PATH):
+	    if not os.path.isdir(os.path.join(SYS_ARG_PATH, name_of_file)):
+	        type_of_file = name_of_file.split('.')[-1]
+	        DIRECTORIES.setdefault(type_of_file, []).append(name_of_file)
+
+	# pprint(DIRECTORIES)
+
+	# Move all files into their folder
+	for name_folder, items_in_folder in DIRECTORIES.items():
+	    folder_path = os.path.join(SYS_ARG_PATH, name_folder)
+	    # If the file does not exist, create new directory
+	    if not os.path.exists(folder_path):
+	        os.mkdir(folder_path)
+
+	    for folder_item in items_in_folder:
+	    	# Get the file name
+	        source = os.path.join(SYS_ARG_PATH, folder_item)
+	        # Move the file to appr. folder
+	        destination = os.path.join(folder_path, folder_item)
+	        # This will print full path of file moving from and to
+	        # print(f'Moving {source} to {destination}')
+	        # Rename the new dir
+	        os.rename(source, destination)
+
+if __name__ == '__main__':
+	#This will keep script running
+	#Every 20 seconds it will scan for new files and if new file is in the 
+	# directory, it will move it to its appropriate place
+	# print(platform.platform())
+	# os.system('clear')
+	# os.system('cls')
+	ask = input("Would you like to keep this program running in the background? Y/N --> ")
+	ask = ask.lower()
+	if ask == 'y':
+		# os.system('clear')
+		# os.system('cls')
+		while True:
+			organize()
+			time.sleep(20)
+	if ask == 'n':
+		# os.system('clear')
+		# os.system('cls')
+		print('=================================================')
+		print('\t\tRUNNING ONCE')
+		print('=================================================')
+		organize()
+
+
+
+		# To Exit when running in the background
+		# press ctrl + c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
